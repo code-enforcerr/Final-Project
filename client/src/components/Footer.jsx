84c9ca1678e98570c2689240feacfd307ddb7303
code enforcerr <code.enforcer26@gmail.com>
2024-09-17 06:12:53 -0700

    Frontend

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <footer className='bg-lime-700 text-lime-300 p-3 text-center bottom-0 w-full'>
+            <p>© 2024 Easy Homes. All rights reserved.</p>
+        </footer>
+    </div>
+  )
+}
+
+export default Footer
